3ee868c39ad8288149d9a792cb5996ca6f5fb4d4
Raja Daoud <rdaoud@google.com>
2015-07-17 18:33:31 -0700

    Syncbase JS tests: use a /tmp dir for syncbase storage.
    This is needed in preparation to integrating the blob store in Syncbase.
    The blob store will create directories/files on disk, so this passes
    Syncbase a root directory under /tmp.
    
    Change-Id: Idff29e737ae95c130102cb0ebabc57c22216ca63

--- test/start-syncbased.sh
+++ test/start-syncbased.sh
@@ -9,5 +9,7 @@
 # does not allow flags or arguments to the executables it starts.  We should
 # fix service-runner to allow flags/arguments, and then have it start syncbased
 # directly with the appropriate flags.  Then we can delete this file.
+# TODO(rdaoud): how to cleanup the tmp test dir; "rm" here doesn't do it.
 
-syncbased -v=1 --name test/syncbased --engine memstore --v23.tcp.address 127.0.0.1:0
+testdir="$(mktemp -d "${TMPDIR:-/tmp}"/sbtest.XXXXXXXX)"
+syncbased -v=1 --name test/syncbased --engine memstore --root-dir "${testdir}" --v23.tcp.address 127.0.0.1:0
